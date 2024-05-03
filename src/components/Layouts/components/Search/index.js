ce956d12169e6acd40a3e6c593e5f0beae6e638d
Nguyen Thanh Danh <nguyenthanhdanh221102@gmail.com>
2024-05-03 16:38:18 +0700

    sử dụng thư viện Axios

--- src/components/Layouts/components/Search/index.js
+++ src/components/Layouts/components/Search/index.js
@@ -6,12 +6,14 @@ import {
   faCircleXmark,
 } from '@fortawesome/free-solid-svg-icons';
 import HeadlessTippy from '@tippyjs/react/headless';
+
+import * as searchService from '~/apiServices/searchService';
 import { Wrapper as PopperWrapper } from '~/components/Popper';
 import AccountItem from '~/components/AccountItem';
 import { SearchIcon } from '~/components/Icon';
+import { useDebounce } from '~/hooks';
 
 import styles from './Search.module.scss';
-import { useDebounce } from '~/hooks';
 
 const cx = classNames.bind(styles);
 
@@ -31,21 +33,16 @@ function Search() {
       return;
     }
 
-    setLoading(true);
+    const fetchApi = async () => {
+      setLoading(true);
 
-    fetch(
-      `https://tiktok.fullstack.edu.vn/api/users/search?q=${encodeURIComponent(
-        debounced
-      )}&type=less`
-    )
-      .then((res) => res.json())
-      .then((res) => {
-        setSearchResult(res.data);
-        setLoading(false);
-      })
-      .catch(() => {
-        setLoading(false);
-      });
+      const result = await searchService.search(debounced);
+      setSearchResult(result);
+
+      setLoading(false);
+    };
+
+    fetchApi();
   }, [debounced]);
 
   const handleClear = () => {
